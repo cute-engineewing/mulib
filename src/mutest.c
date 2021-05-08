b1fd73f3d3751ccfcbaed70e89c341a9fe667ddd
supercyp <supercyp@protonmail.com>
2021-05-08 09:20:57 -0400

    test: now run the test in a process

--- src/mutest.c
+++ src/mutest.c
@@ -1,13 +1,31 @@
 #include "mulib/mutest.h"
 #include <stdio.h>
+#include <sys/wait.h>
+
+#include <unistd.h>
+
+int run_threaded_test(struct mutest_test test)
+{
+    pid_t child;
+    int status;
+    fflush(stdout); 
+    child=fork();
+    if (child==0)
+    {
+        exit(test.func());        
+    }
+    waitpid(child, &status, 0);
+
+    return status;
+}
 
 int
 mutest_run_single_test(struct mutest_test test)
 {
 	int result;
 	printf("test: %-30s | ", test.name);
-
-	result = test.func();
+    
+    result = run_threaded_test(test);
 
 	if (result != MUTEST_SUCCESS) /* we want a success */
 	{
