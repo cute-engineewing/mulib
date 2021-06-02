258f275078c404d8d0a382d7e66fea8ee876ef2d
supercyp <supercyp@protonmail.com>
2021-06-02 15:12:14 -0400

    feat: added mustr_find_table_entry

--- src/str.c
+++ src/str.c
@@ -12,5 +12,20 @@ is_string_a_number(const char *str)
 			return false;
 		}
 	}
+
 	return true;
+}
+
+int
+mustr_find_table_entry(const char *entry_name, const char **table,
+					   size_t table_size)
+{
+	for (size_t i = 0; i < table_size; i++)
+	{
+		if (strcmp(table[i], entry_name) == 0)
+		{
+			return i;
+		}
+	}
+	return STR_TABLE_NOT_FOUNDED;
 }
